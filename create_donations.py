21dffc170d40b0a8d2ae0413ddbdb8dc52d31fb6
Jeff James <jeffjames@gmail.com>
2024-11-04 16:44:16 -0800

    Add report and clean up

--- create_donations.py
+++ create_donations.py
@@ -1,67 +1,38 @@
-# import csv
+import datetime
 import pandas as pd
 import json
 
-# donation_dates = []
+output_donations = open("hub_owner_time_donations.json", "w", encoding='utf-8')
+
+output_report = open("hub_owner_time_donations_report.txt", "w", encoding='utf-8')
 
 def write_record(person_id, hub_id, donation_date):
     record = {'donation_item_id': 40013, 'person_id': person_id, 'donation_date': donation_date, 'donation_qty': 5, 'time_value': 5, 'donor_hub_id': hub_id}
     json.dump(record, output_donations, ensure_ascii=False, indent=4)
 
-output_donations = open("hub_owner_time_donations.json", "w", encoding='utf-8')
-
-
-# with open('donation_dates.csv', 'r') as file:
-#     csv_reader = csv.reader(file)
-#     for row in csv_reader:
-#         if row[0] < '11/1/2024':
-#             donation_dates.append(row[0])
-#             print(row)
-
+def write_report(person_id, hub_id, donation_date):
+    line = 'person_id: ' + person_id.__str__() + ', donation_date: ' + donation_date.__str__() + ', hub_id: ' + hub_id.__str__() + '\n'
+    output_report.writelines(line)
 
 donation_dates = pd.read_csv("donation_dates.csv", parse_dates=["donation_date"])
-# print(donation_dates)
-
-# for date in donation_dates:
-#     print(date)
-
 
 # Data in owner_dates is hub_id, person_id, owner_start_date, owner_end_date, hub_close_ date
-
-# with open('owner_dates.csv', encoding='utf-8-sig') as file:
-#     csv_reader = csv.reader(file)
-#     for row in csv_reader:
-#         # print(row[0])
-#         # print(row[1])
-#         # donation_dates.append(row[0])
-#         if row[2] != None:
-#             for date in donation_dates:
-#                 if date >= row[2]:
-#                     # if row[3] == None and row[4] == None:
-#                         # write_record(row[1], row[0], date)
-#                         x = date
-
-
-# owner_dates = pd.read_csv("owner_dates.csv", parse_dates=["owner_start_date", "owner_end_date", "hub_close_date"], dtype={'owner_start_date': 'datetime64[ns]', 'owner_end_date': 'datetime64[ns]', 'hub_close_date': 'datetime64[ns]'})
 owner_dates = pd.read_csv("owner_dates.csv", parse_dates=["owner_start_date", "owner_end_date", "hub_close_date"])
-# owner_dates = pd.read_csv("owner_dates.csv")
-# Something about the end date column causes parse_dates to fail, fix it here
-owner_dates['owner_end_date'] = pd.to_datetime(owner_dates['owner_end_date'], errors='coerce')
-print(owner_dates)
-for owner in owner_dates.itertuples():
-    # print(owner)
-    if not pd.isnull(owner.owner_start_date):
-        # print(owner.owner_start_date)
-        for date in donation_dates.itertuples():
-            if not pd.isnull(owner.owner_end_date):
-                if date.donation_date >= owner.owner_start_date and date.donation_date <= owner.owner_end_date and date.donation_date <= owner.hub_close_date:
-                    # print(date.donation_date)
-                    # print(type(date.donation_date))
-                    # print('End date type: ')
-                    # print(type(owner.owner_end_date))
-                    # x = date
-                    # write_record(owner.person_id, owner.hub_id, date.__str__())
-                    write_record(owner.person_id, owner.hub_id, date.donation_date.__str__())
 
+# Something about the end date column causes parse_dates to fail, so fix it here
+owner_dates['owner_end_date'] = pd.to_datetime(owner_dates['owner_end_date'], errors='coerce')
+
+for owner in owner_dates.itertuples():
+    end_date = datetime.datetime.now()
+    if not pd.isna(owner.owner_end_date):
+        end_date = owner.owner_end_date
+    if not pd.isna(owner.hub_close_date):
+        if owner.hub_close_date < end_date:
+            end_date = owner.hub_close_date
+        
+    for date in donation_dates.itertuples():
+        if date.donation_date >= owner.owner_start_date and date.donation_date <= end_date:
+            write_record(owner.person_id, owner.hub_id, date.donation_date.__str__())
+            write_report(owner.person_id, owner.hub_id, date.donation_date.__str__())
 
 output_donations.close()
